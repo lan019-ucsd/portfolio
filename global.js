a6b8730e49ccdf4b2c13876b1ab4f87984cd26d4
lan019-ucsd <lan019@ucsd.edu>
2025-10-16 12:24:00 -0700

    Global change

--- global.js
+++ global.js
@@ -4,10 +4,37 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+const BASE_PATH = 
+location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+  ? '/' 
+  : '/portfolio/';
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact'},
+  { url: 'cv/', title: 'CV'},
+  { url: 'https://github.com/lan019-ucsd/portfolio/', title: 'Profile'}
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+
 let navLinks = $$('nav a');
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
+for (let a of navLinks) { 
+  a.classList.toggle( 
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+}
 
 currentLink?.classList.add('current');
