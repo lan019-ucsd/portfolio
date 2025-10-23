c8f3dc19a01e69e062ac69c79b2bb0919157151f
lan019-ucsd <lan019@ucsd.edu>
2025-10-23 12:16:56 -0700

    Style change

---
+++ index.js
@@ -0,0 +1,11 @@
+import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
+
+(async () => { 
+    const projects = await fetchJSON('./lib/projects.json');
+
+    const latestProjects = projects.slice(0, 3);
+
+    const projectsContainer = document.querySelector('.projects');
+
+    renderProjects(latestProjects, projectsContainer, 'h2');
+})
