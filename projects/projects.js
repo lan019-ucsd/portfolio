72f2d82cc0a2327c3d014148c0fcd3a3729e4813
lan019-ucsd <lan019@ucsd.edu>
2025-10-30 14:49:07 -0700

    Project Changes

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,5 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 import { fetchJSON, renderProjects } from '../global.js';
 
 (async () => {
@@ -12,3 +14,12 @@ import { fetchJSON, renderProjects } from '../global.js';
         titleElement.textContent += `(${projects.length})`;
     }
 })();
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
