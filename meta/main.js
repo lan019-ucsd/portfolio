c7b9bb2bc098f58cbcebb55b7524da7e1a45962f
lan019-ucsd <lan019@ucsd.edu>
2025-11-06 15:40:49 -0800

    Meta Change

--- meta/main.js
+++ meta/main.js
@@ -1,11 +1,85 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
+console.log('main.js loaded');
+
 async function loadData() {
-  const data = await d3.csv('loc.csv');
-  console.log(data);
+  const data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+
   return data;
 }
 
-let data = await loadData();
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
 
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        enumerable: false,
+        writable: false,
+        configurable: true,
+      });
+
+      return ret;
+    });
+}
+
+function renderCommitInfo(data, commits) {
+  // Create the dl element
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  // Add total LOC
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  // Add total commits
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+
+  dl.append('dt').text('Number of files');
+  dl.append('dd').text(d3.groups(data, d => d.file).length);
+
+  dl.append('dt').text('Longest file (lines)');
+  const fileLengths = d3.rollups(
+    data,
+    v => d3.max(v, d => d.line),
+    d => d.file
+  );
+
+  const longestFile = d3.greatest(fileLengths, d => d[1]);
+  dl.append('dd').text(`${longestFile[0]} (${longestFile[1]} lines)`);
+
+  dl.append('dt').text('Average file length');
+  const avgFileLength = d3.mean(fileLengths, d => d[1]);
+  dl.append('dd').text(avgFileLength.toFixed(1));
+
+  dl.append('dt').text('Max depth');
+  dl.append('dd').text(d3.max(data, d => d.depth))
+}
+
+const data = await loadData();
+const commits = processCommits(data);
+console.log(commits);
+
+renderCommitInfo(data, commits);
 
