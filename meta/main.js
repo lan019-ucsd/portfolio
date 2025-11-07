25c30f0c058cf9874cfc15483157e4567d235757
lan019-ucsd <lan019@ucsd.edu>
2025-11-06 16:13:14 -0800

    Meta Change

--- meta/main.js
+++ meta/main.js
@@ -80,9 +80,62 @@ function renderCommitInfo(data, commits) {
   dl.append('dd').text(avgFileLength.toFixed(1));
 }
 
+function renderScatterPlot(data, commits) { 
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, right: 10, bottom: 10, left: 50};
+
+  const useableArea = { 
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+    .nice();
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, 24])
+    .range([height, 0]);
+
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom}`)
+    .call(xAxis)
+
+  svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', (d) => xScale(d.datetime))
+    .attr('cy', (d) => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+}
+
 const data = await loadData();
 const commits = processCommits(data);
 console.log(commits);
 
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
 
